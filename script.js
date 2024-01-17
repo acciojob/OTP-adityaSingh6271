05edbd6d3ce6c156938595d2e71d5fd89f58cc61
Aditya Singh <131813027+adityaSingh6271@users.noreply.github.com>
2024-01-17 12:37:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function focusNextInput(currentInput) {
+            if (currentInput.value.length === 1) {
+                const nextInput = currentInput.nextElementSibling;
+                if (nextInput) {
+                    nextInput.focus();
+                }
+            }
+        }
+
+        function focusPrevInput(currentInput, event) {
+            if (event.key === 'Backspace' && currentInput.value.length === 0) {
+                const prevInput = currentInput.previousElementSibling;
+                if (prevInput) {
+                    prevInput.focus();
+                }
+            }
+        }
